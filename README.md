aabe28f27bf2e458a58a86b5f9fce3b12f455c76
apple <apple@qt-muneeb-pc.local>
2023-05-30 10:48:24 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Description
+This is my new personal portfolio updated Version
+here is the live site link => comming soon
---
+++ src/content_option.js
@@ -0,0 +1,162 @@
+const logotext = "Muneeb ur rehman";
+const meta = {
+    title: "Muneeb ur rehman",
+    description: "I’m Muneeb ur rehman data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Muneeb ur rehman",
+    animated: {
+        first: "Cutting-Edge Mobile App Developer",
+        second: "Innovative Website Developer",
+        third: "Powerful Backend App Specialist"
+    },
+    description: "Experienced software engineer passionate about developing innovative programs to drive organizational success. Proficient in leveraging technology to create reliable and user-friendly systems. Skilled leader with a track record of motivating and managing teams to deliver high-quality software solutions. A confident communicator and strategic thinker, dedicated to customizing software to meet organizational needs, showcase core competencies, and drive continued success.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "Experienced full stack mobile app developer with a dynamic and personable approach. Specialized in cross-platform mobile development with React Native, native Android development with Kotlin and Java. Proficient in server-side, front-end, and database languages such as JavaScript, React.js, Swift, NodeJS, Mongo DB, Firebase, SQL & SQLite. Excellent English communication skills and a strong drive to continuously learn new technologies. ",
+};
+const achievement = [{
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+},
+{
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+},
+{
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+},
+];
+
+const skills = [{
+    name: "React native",
+    value: 90,
+},
+{
+    name: "Javascript",
+    value: 80,
+},
+{
+    name: "ReactJs",
+    value: 85,
+},
+{
+    name: "NextJs",
+    value: 75,
+},
+{
+    name: "Backend Technologies (NodeJs)",
+    value: 85,
+},
+{
+    name: "Database",
+    value: 70,
+},
+];
+
+const experiances = [{
+    title: "Senior Mobile App Engineer at ScalaMed (Cardinal Health) ",
+    description: "During my tenure at ScalaMed, a US-based product recently acquired by Cardinal Health, I gained extensive experience and took on various roles, utilizing cutting-edge technologies. I actively participated in design discussions, shaping application architecture, and implementing new features. Notably, I successfully transformed an expo-based app to bare React Native, achieving a bug-free app through the use of the MST state library and Jest for Test Driven Development. I significantly improved app performance, reduced load time, integrated React Native debugging tools, expanded the user base with multi-language support, and enhanced the UI. Following agile methodologies, I ensured meticulous planning, coding, and debugging, prioritized security compliance, implemented code push for OTA updates, and deployed applications to app stores. Throughout my time at ScalaMed, I demonstrated adaptability and technical prowess, achieving performance improvements, integrating essential debugging tools, expanding the user base, and refining the UI. I oversaw the software development lifecycle, collaborated with stakeholders, upheld security compliance, and implemented efficient over-the-air updates. My outstanding contributions earned me nominations for Employee of the Month and Best Performance of the Year.",
+},
+{
+    title: "Full Stack Mobile & Web Developer | Team Leader",
+    description: "During my 1.5-year tenure at my current organization, I have gained extensive experience in diverse roles and technologies. I have designed, developed, and modified over 15 software systems, showcasing my proficiency in software development. With my skills in React Native, I have created visually appealing and user-friendly applications by developing pixel-perfect user interfaces. Collaboration has been a key aspect of my work, as I have effectively worked with project managers, quality assurance testers, and the Chief Technology Officer (CTO) to deliver high-quality projects. I have also empowered and upskilled staff members, enabling them to make independent updates. Additionally, I have successfully managed time-sensitive updates for live production sites and apps, ensuring their smooth functioning. I have overcome limitations in React Native by developing native modules from scratch, expanding the functionality of the applications.      In my role, I have prioritized web application security, resolved bugs, and enhanced functionality to improve the overall user experience. As a project overseer, I have managed the entire software development lifecycle, meeting deadlines and achieving organizational objectives. Client satisfaction has been central to my work, and I have streamlined the development and deployment processes for efficient updates and deployments.",
+},
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/550/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "muneebkhann0@gmail.com",
+    YOUR_FONE: "+923171511383",
+    description: "Get in touch with me for any inquiries or collaboration opportunities. I'm just an email away, ready to discuss how we can work together to achieve your goals.",
+    YOUR_SERVICE_ID: "gmail",
+    YOUR_TEMPLATE_ID: "template_ltJG77Zs",
+    YOUR_USER_ID: "user_Cd6cR9lwxOnCLwbWFkI3A",
+};
+
+const socialprofils = {
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/muneeb-ur-rehman-212619148/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    achievement,
+    skills,
+    experiances,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
